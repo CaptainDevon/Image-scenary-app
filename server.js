44194dfea9fd2333d0879628eea9df861f1286c7
Divyansh Sinha <114439802+CaptainDevon@users.noreply.github.com>
2024-06-18 11:54:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,18 +10,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-  // res.writeHead(200,{'Content-Type': 'text/html'})
-  // res.writeHead(200,{'Content-Type': 'text/stylesheet'})
   res.sendFile(path.join(__dirname, "/login_page.html"));
 });
 app.get("/signUp", function (req, res) {
-  // res.writeHead(200,{'Content-Type': 'text/html'})
-  // res.writeHead(200,{'Content-Type': 'text/stylesheet'})
   res.sendFile(path.join(__dirname, "/signup_page.html"));
 });
 app.get("/home", function (req, res) {
-    // res.writeHead(200,{'Content-Type': 'text/html'})
-    // res.writeHead(200,{'Content-Type': 'text/stylesheet'})
     res.sendFile(path.join(__dirname, "/main.html"));
   });
 app.post("/signUp", signUp);
